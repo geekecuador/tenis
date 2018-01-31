a8728d25d51dcd5454fdf7117a468975a2c4dc31
David Pulloquinga <dapulloqquinga@espe.edu.ec>
2018-01-31 14:48:47 -0500

    da

--- ctc/admin.py
+++ ctc/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Publicacion,Categoria
+from .models import Publicacion,Categoria,Circular
 # Register your models here.
 admin.site.register(Publicacion)
 admin.site.register(Categoria)
+admin.site.register(Circular)
