ae4117eca11da33ee2e84b43e3df012455ed76a0
David Pulloquinga <dapulloqquinga@espe.edu.ec>
2018-01-31 14:26:27 -0500

    d

--- ctc/models.py
+++ ctc/models.py
@@ -3,10 +3,13 @@ from django.db import models
 # Create your models here.
 class Categoria(models.Model):
     name = models.CharField(max_length=200)
-
+    def __str__(self):
+        return self.name
 class Publicacion(models.Model):
     titulo = models.CharField(max_length=200)
     imagen = models.ImageField(upload_to='imagenes/')
     contenido = models.TextField()
     categoria = models.ForeignKey(Categoria,  on_delete='cascade')
     fecha = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.titulo
