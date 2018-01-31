6c34215efd0dfa5ac5195f7c69ef327c8b5d8a77
David Pulloquinga <dapulloqquinga@espe.edu.ec>
2018-01-31 14:47:35 -0500

    da

--- ctc/views.py
+++ ctc/views.py
@@ -7,9 +7,10 @@ from .models import Categoria, Publicacion, Circular
 
 def publicacion(request, idpost):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
+    circular = Circular.objects.get(titulo='circular')
     publicacion = Publicacion.objects.get(id=idpost)
     return render(request, 'publicacion.html',{
-        "publicacion": publicacion,"publicaciones":publicaciones
+        "publicacion": publicacion,"publicaciones":publicaciones,"circular":circular
     })
 
 def mensaje(request):
@@ -25,84 +26,101 @@ def mensaje(request):
         ['correo@cotopaxitenisclub.com'],
         fail_silently=False,)
         publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-        return render(request, 'mensaje.html', {"publicaciones": publicaciones})
+        circular = Circular.objects.get(titulo='circular')
+        return render(request, 'mensaje.html', {"publicaciones": publicaciones,"circular":circular})
     else:
         return redirect('/')
 
 
 def index(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'index.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'index.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def contacto(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'contacto.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'contacto.html', {"publicaciones":publicaciones,"circular":circular})
 
 def blog(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'blog.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'blog.html', {"publicaciones":publicaciones,"circular":circular})
 
 def cotopaxi(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'cotopaxi.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'cotopaxi.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def daviscup(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'daviscup.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'daviscup.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def directorio(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'directorio.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'directorio.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def dobles(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'dobles.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'dobles.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def estatutos(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'estatutos.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'estatutos.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def eventos(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'eventos.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'eventos.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def historia(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'historia.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'historia.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def master(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'master.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'master.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def presidentes(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'presidentes.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'presidentes.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def ranking(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'ranking.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'ranking.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def reinas(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'reinas.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'reinas.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def tenis(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'tenis.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'tenis.html', {"publicaciones":publicaciones,"circular":circular})
 
 
 def contacto(request):
     publicaciones = Publicacion.objects.order_by("-fecha")[:5]
-    return render(request, 'contacto.html', {"publicaciones":publicaciones})
+    circular = Circular.objects.get(titulo='circular')
+    return render(request, 'contacto.html', {"publicaciones":publicaciones,"circular":circular})
